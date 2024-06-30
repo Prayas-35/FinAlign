18c74e06ec57d81926429dc6a9984485ab12b934
deba7 <debayudhbasu@gmail.com>
2024-06-30 23:38:16 +0530

    finalest commit

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -25,9 +25,6 @@ const Header = () => {
         </Link>
 
         <nav className="hidden lg:flex items-center gap-6">
-          <Link to="#" className="text-white hover:text-gray-300">
-            About Us
-          </Link>
           {token ? (
             <>
               <Link to="/profile" className="text-white hover:text-gray-300">
@@ -77,13 +74,6 @@ const Header = () => {
 
       {menuOpen && (
         <nav className="lg:hidden bg-background p-4 shadow-md">
-          <Link
-            to="#"
-            className="block text-white hover:text-gray-300 mb-2"
-            onClick={toggleMenu}
-          >
-            About Us
-          </Link>
           {token ? (
             <>
               <Link
@@ -91,21 +81,15 @@ const Header = () => {
                 className="block text-white hover:text-gray-300 mb-2"
                 onClick={toggleMenu}
               >
-               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
-                </svg>
+               Profile
               </Link>
               <Link to='/stocks'
                className="block text-white hover:text-gray-300 mb-2"
-               onClick={toggleMenu}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-               <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125" />
-             </svg>
+               onClick={toggleMenu}> Stocks
               </Link>
               <Link to='/analytics'
                className="block text-white hover:text-gray-300 mb-2"
-               onClick={toggleMenu}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-               <path strokeLinecap="round" strokeLinejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 0 1 3 19.875v-6.75ZM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V8.625ZM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V4.125Z" />
-             </svg>             
+               onClick={toggleMenu}> Analytics
               </Link>
 
               <button
@@ -115,9 +99,7 @@ const Header = () => {
                 }}
                 className="block text-white hover:text-gray-300 mb-2"
               >
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
-</svg>
+               Logout
 
               </button>
             </>
