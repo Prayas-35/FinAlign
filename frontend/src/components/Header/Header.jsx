8defb613970946018cd826a34958c6660a85e232
Prayas <prayaspal04@gmail.com>
2024-07-02 16:57:12 +0530

    profile

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -6,6 +6,7 @@ const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const { token, logout } = useContext(UserContext);
   const [profileOpen, setProfileOpen] = useState(false);
+  const [username, setUsername] = useState('');
   const navigate = useNavigate();
 
   const toggleMenu = () => {
@@ -38,6 +39,7 @@ const Header = () => {
 
         const data = await response.json();
         console.log("Profile fetched:", data.username);
+        setUsername(data.username);
       } catch (error) {
         console.error("Error fetching profile:", error.message);
       }
@@ -63,6 +65,11 @@ const Header = () => {
                   <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                 </svg>
               </button>
+              {profileOpen && (
+                <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg z-20">
+                  <div className="p-2 text-gray-800">{username}</div>
+                </div>
+              )}
               <Link to='/stocks' className="text-white hover:text-gray-300">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125" />
